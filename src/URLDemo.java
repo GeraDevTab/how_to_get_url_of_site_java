7ec7db8b4f6a8bc22b0f9cf0c7d905b943642a71
Gerardo <everythingissofast@gmail.com>
2025-06-06 08:21:42 -0600

    nuevo objeto URL e impresion de sus metodos

--- src/URLDemo.java
+++ src/URLDemo.java
@@ -1,11 +1,23 @@
 import java.net.InetAddress;
+import java.net.URL;
 
 public class URLDemo {
     public static void main(String[] args) {
         try{
+            URL url = new URL("https://www.bachoco.com");
+
+            System.out.println("Protocol: " + url.getProtocol());
+            System.out.println("Host Name: " + url.getHost());
+            System.out.println("Port Number: " + url.getPort());
+            System.out.println("File Name: " + url.getFile());
+
+            /*
             InetAddress ip = InetAddress.getByName("bachoco.com");
             System.out.println("Host Name: " + ip.getHostName());
             System.out.println("IP Address: " + ip.getHostAddress());
+
+             */
+
         }
         catch (Exception e){
             System.out.println(e);
